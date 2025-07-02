0e639663f7fda4f4c1eb00a9b4d3c0eef4ec48bb
xuyenchi <xuyenltn1996@gmail.com>
2025-07-02 10:16:53 +0700

    Framework Manage Environment

--- src/main/java/utilities/PropertiesConfig.java
+++ src/main/java/utilities/PropertiesConfig.java
@@ -1,4 +1,53 @@
 package utilities;
 
+import commons.GlobalConstants;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertiesConfig {
+    private Properties properties;
+    private final String propertyFilePath = GlobalConstants.ENVIRONMENT_CONFIG_PATH + "%s.properties";
+
+    public static PropertiesConfig getProperties(String serverName) {
+        return new PropertiesConfig(serverName);
+    }
+
+    public PropertiesConfig(String serverName) {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(String.format(propertyFilePath, serverName)));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getApplicationUrl() {
+        String url = properties.getProperty("App.URL");
+        if(url != null) return url;
+        else throw new RuntimeException("Url not specified in the properties file.");
+    }
+
+    public String getApplicationUserName() {
+        String username = properties.getProperty("App.User");
+        if(username != null) return username;
+        else throw new RuntimeException("Username not specified in the properties file.");
+    }
+
+    public String getApplicationPassword() {
+        String password = properties.getProperty("App.Pass");
+        if(password != null) return password;
+        else throw new RuntimeException("Password not specified in the properties file.");
+    }
 }
---
+++ src/test/java/com/nopcomerce/user/User_01_Lv23_Environment.java
@@ -0,0 +1,116 @@
+package com.nopcomerce.user;
+
+import commons.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.nopcomerce.PageGenerator;
+import pageObjects.nopcomerce.user.*;
+import pojo.UserDataJson;
+import utilities.PropertiesConfig;
+
+public class User_01_Lv23_Environment extends BaseTest {
+    WebDriver driver;
+    private UserHomePO homePage;
+    private UserRegisterPO registerPage;
+    private UserMyAccountPO myAccountPage;
+    private UserAddressBookPO addressBookPage;
+    private UserOrderPO orderPage;
+    public UserDataJson userDataJson;
+    private String emailAddress, fullName;
+    private PropertiesConfig propertiesConfig;
+
+
+    public User_01_Lv23_Environment() {
+        super();
+    }
+
+    @Parameters({"browser","environment"})
+    @BeforeClass
+    public void beforeClass(String browserName, String environment) {
+        propertiesConfig = PropertiesConfig.getProperties(environment);
+        driver = getBrowserName(browserName, propertiesConfig.getApplicationUrl());
+        homePage = PageGenerator.getHomePage(driver);
+        userDataJson = UserDataJson.getUser();
+        emailAddress = userDataJson.getEmailAddress() + generateRandomNumber() + "@gmail.com";
+        fullName = userDataJson.getFirstName() + " " + userDataJson.getMiddleName() + " " + userDataJson.getLastName();
+
+    }
+
+    @Description("Register new account")
+    @Severity(SeverityLevel.CRITICAL)
+    @Test
+    public void TC_01_Register_Techpanda() {
+
+        registerPage = homePage.openRegisterPage();
+        Assert.assertEquals(homePage.getTextpageTitle(), "CREATE AN ACCOUNT");
+        //qua trang register
+
+        registerPage.enterTextboxByID(driver, "firstname", userDataJson.getFirstName());
+        registerPage.enterTextboxByID(driver, "middlename", userDataJson.getMiddleName());
+        registerPage.enterTextboxByID(driver, "lastname", userDataJson.getLastName());
+        registerPage.enterTextboxByID(driver, "email_address", emailAddress);
+        registerPage.enterTextboxByID(driver, "password", userDataJson.getPassword());
+        registerPage.enterTextboxByID(driver, "confirmation", userDataJson.getPassword());
+
+        myAccountPage = registerPage.clickRegisterButon();
+
+        //qua trang my account
+        Assert.assertEquals(myAccountPage.getSuccessRegister(), "Thank you for registering with Main Website Store.");
+        System.out.printf("---Asert Fullname---");
+        System.out.println("Get fullname system : " + myAccountPage.getContactInfo());
+        System.out.println("Get fullname variable : " + fullName);
+        Assert.assertTrue(myAccountPage.getContactInfo().contains(fullName));
+        System.out.println("---Assert Email---");
+        Assert.assertTrue(myAccountPage.getContactInfo().contains(userDataJson.getEmailAddress()));
+
+
+    }
+
+    // @Test
+    public void TC_02_MyAccount() throws InterruptedException {
+        myAccountPage.clickAccountLink();
+        myAccountPage = myAccountPage.openMyAccountPage();
+        Thread.sleep(3000);
+    }
+
+    // @Test
+    public void TC_03_Switch_Page() {
+        //Myaccount -> Adrress
+        addressBookPage = (UserAddressBookPO) myAccountPage.openSidebarLinkByPageName("Address Book");
+
+        //Adress -> Order
+        orderPage = (UserOrderPO) addressBookPage.openSidebarLinkByPageName("My Orders");
+        // Order -> My account
+        myAccountPage = (UserMyAccountPO) orderPage.openSidebarLinkByPageName("Account Dashboard");
+        ;
+
+    }
+
+    // @Test
+    public void TC_04_Switch_Page() {
+        //Myaccount -> Adrress
+        myAccountPage.openSidebarLinkByPageName("Address Book");
+        addressBookPage = PageGenerator.getUserAddressPage(driver);
+
+        //Adress -> Order
+        addressBookPage.openSidebarLinkByPageName("My Orders");
+        orderPage = PageGenerator.getUserOrderPage(driver);
+        // Order -> My account
+        orderPage.openSidebarLinkByPageName("Account Dashboard");
+        myAccountPage = PageGenerator.getUserMyAccountPage(driver);
+        ;
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void afterClass() {
+        closeBrowserDriver();
+    }
+}
